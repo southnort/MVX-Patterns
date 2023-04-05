ed90e905e730ad08c8446fda93212042e20889c8
southnort <south-nort@ya.ru>
2023-04-05 15:36:44 +0300

    MVP-P example

---
+++ Assets/MVP-PassiveView/ClampedAmountWithIcon.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using TMPro;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MVP_PassiveView
+{
+    public class ClampedAmountWithIcon : MonoBehaviour
+    {
+        [SerializeField] private Image _icon;
+        [SerializeField] private TextMeshProUGUI _text;
+        [SerializeField] private Button _clickSource;
+
+        public event Action Click;
+
+        private void OnEnable()
+        {
+            try
+            {
+                Validate();
+            }
+            catch (Exception e)
+            {
+                enabled = false;
+                throw e;
+            }
+
+            _clickSource.onClick.AddListener(OnClick);
+        }
+
+        private void OnDisable()
+        {
+            _clickSource?.onClick.RemoveListener(OnClick);
+        }
+
+        private void Validate()
+        {
+            if (_icon == null)
+                throw new InvalidOperationException();
+
+            if (_text == null)
+                throw new InvalidOperationException();
+        }
+
+        public void SetAmount(int amount, int maxAmount)
+        {
+            _text.text = $"{amount}";
+            _icon.color = Color.Lerp(Color.white, Color.red, amount / maxAmount);
+        }
+
+        private void OnClick()
+        {
+            Click?.Invoke();
+        }
+    }
+}
