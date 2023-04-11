4db2c8c19256dcb7b13751a31c2bcbd15993410d
southnort <south-nort@ya.ru>
2023-04-11 12:48:36 +0300

    MVVM Pattern

---
+++ Assets/MVVM/WalletViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace MVVM
+{
+    public class WalletViewModel:ViewModel
+    {
+        [Model] private Wallet _wallet = new Wallet();
+
+        [Project] public int Coins => _wallet.Coins;
+
+        [Command] public void AddCoin() => _wallet.AddCoin();
+
+    }
+}
